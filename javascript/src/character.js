fa2363d1361ef97c9cfb4476f407aec010fee3b6
Carlos Castillo <castillo.st@gmail.com>
2023-06-23 18:47:34 +0200

    Refactor: use constants to remove magic numbers

--- javascript/src/character.js
+++ javascript/src/character.js
@@ -1,7 +1,10 @@
+const STARTING_HEALTH_POINTS = 1000;
+const STARTING_LEVEL = 1;
+
 class Character {
   constructor() {
-    this.healthPoints = 1000;
-    this.level = 1;
+    this.healthPoints = STARTING_HEALTH_POINTS;
+    this.level = STARTING_LEVEL;
     this.isAlive = true;
   }
 }
