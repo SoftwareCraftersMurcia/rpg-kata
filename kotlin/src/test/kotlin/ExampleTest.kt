9ed789ed3354d129d37a13767b50c7ae952caf25
F0rno <pforper2412@g.educaand.es>
2023-06-23 18:30:26 +0200

    Class character

--- kotlin/src/test/kotlin/ExampleTest.kt
+++ kotlin/src/test/kotlin/ExampleTest.kt
@@ -2,11 +2,13 @@ import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.DynamicTest
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestFactory
+import javax.xml.stream.events.Characters
 
 class ExampleTest {
 
     @Test
     fun change_me_first() {
+        val character = Character()
         Assertions.assertEquals("Example", "Example")
     }
 
