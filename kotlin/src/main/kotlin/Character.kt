b0e4d44fda6116ac765682dcc770b4dbaf6f326f
F0rno <pforper2412@g.educaand.es>
2023-06-23 19:27:35 +0200

    Passing for refactor

--- kotlin/src/main/kotlin/Character.kt
+++ kotlin/src/main/kotlin/Character.kt
@@ -2,10 +2,7 @@ class Character {
 
     private var health = 1000
     fun hasHealth(value: Int): Boolean = health == value
-    fun hasLevel(): Int {
-        return 1
-    }
-
+    fun hasLevel(): Int = 1
     fun isAlive(): Boolean = health > 0
 
     fun damage(value: Int) {
