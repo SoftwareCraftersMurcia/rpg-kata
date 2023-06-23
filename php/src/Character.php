0493c3ad37d4c41e2847bd176ebe8ebebd8f1423
Chemaclass <chemaclass@outlook.es>
2023-06-23 19:07:13 +0200

    kills_character_is_not_alive

--- php/src/Character.php
+++ php/src/Character.php
@@ -6,25 +6,26 @@ class Character
 {
     private int $health;
     private int $level;
-    private bool $isAlive;
 
     public function __construct(
         int $health = 1000,
-        int $level = 1,
-        bool $isAlive = true
+        int $level = 1
     ) {
         $this->health = $health;
         $this->level = $level;
-        $this->isAlive = $isAlive;
     }
 
     public function receiveDamage(int $damage): void
     {
         if ($this->health < $damage) {
             $this->health = 0;
-            $this->isAlive = false;
             return;
         }
         $this->health -= $damage;
+    }
+
+    public function isAlive(): bool
+    {
+        return $this->health > 0;
     }
 }
--- php/tests/CharacterTest.php
+++ php/tests/CharacterTest.php
@@ -11,7 +11,7 @@ class CharacterTest extends TestCase
     public function instance_of_character_has_good_start_values(): void
     {
         $character = new Character();
-        $characterTest = new Character(1000, 1, true);
+        $characterTest = new Character(1000, 1);
 
         self::assertEquals($characterTest, $character);
     }
@@ -20,7 +20,7 @@ class CharacterTest extends TestCase
     public function damage_subtract_health_from_character(): void
     {
         $character = new Character();
-        $damagedCharacter = new Character(750, 1, true);
+        $damagedCharacter = new Character(750, 1);
 
         $character->receiveDamage(250);
 
@@ -28,14 +28,24 @@ class CharacterTest extends TestCase
     }
 
     /** @test */
-    public function killer_damage_kills_character(): void
+    public function kills_character_without_health(): void
     {
         $character = new Character();
-        $deadCharacter = new Character(0, 1, false);
+        $deadCharacter = new Character(0, 1);
 
         $character->receiveDamage(500);
         $character->receiveDamage(501);
 
         self::assertEquals($deadCharacter, $character);
+    }
+
+    /** @test */
+    public function kills_character_is_not_alive(): void
+    {
+        $character = new Character();
+        $character->receiveDamage(500);
+        self::assertTrue($character->isAlive());
+        $character->receiveDamage(501);
+        self::assertFalse($character->isAlive());
     }
 }
